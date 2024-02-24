d7cd01b9318274362a78e8243ad29fc3293c2049
Erin Shien <erinshiensmith@gmail.com>
2024-02-24 18:17:44 +0000

    wrote plan as pseudocode

---
+++ main.js
@@ -0,0 +1,31 @@
+// Declare url variable
+const url = "https://opentdb.com/api.php?amount=1&category=27&type=boolean";
+
+// Create function that fetches data for 1 true/false question
+// Fetch data
+// If the promise is unfulfilled return an error message
+// Log fetched data
+
+// Declare a variable for the JSON statement that is returned
+
+// Create an event listener that triggers the fetch function when the "new question" button is clicked
+// Display the question in the DOM
+// Include line that changes the message to "please submit your answer"
+
+// Declare a variable for the user's answer
+
+// Create an event listener for when the user clicks the "true" button
+// The user's answer is now true
+// Call the answer check function
+
+// Create an event listener for when the user clicks the "false" button
+// The user's answer is now false
+// Call the answer check function
+
+// Create a function that checks if the user's answer matches the API listed answer
+// Update the "answer" paragraph element to include the answer
+// If the answer matches, change the message to "well done", increase score by 1
+// If the answer does not match, change the message to "better luck next time"
+
+// Create an event listener that changes the message to "please click the "new question" button to receive your first question" when the DOM is loaded
+
